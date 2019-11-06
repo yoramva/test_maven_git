e730cc90aefdff83046c9b83f6f0f78c4e396b12
winrunner winrunner <yoramva@gmail.com>
2019-11-06 11:59:25 +0200

    123

---
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,20 @@
+package guru99.gittest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	protected WebDriver driver;
+	@Test
+	public void myt() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", ".//Drivers//chromedriver.exe");
+		 WebDriver driver = new ChromeDriver();
+		 //driver.get("http://www.google.com");
+		 driver.get("http://www.google.com");
+		 driver.findElement(By.name("q")).sendKeys("abcdefg");
+	}
+}
